09e4e00e32ae085107dd77dda37bc261a56be333
lgaid <104643045+lgaid@users.noreply.github.com>
2022-09-18 12:51:54 -0400

    IoC

--- lab-01-IoC/src/main/java/com/cydeo/Main.java
+++ lab-01-IoC/src/main/java/com/cydeo/Main.java
@@ -2,6 +2,7 @@ package com.cydeo;
 
 import com.cydeo.accunt.Current;
 import com.cydeo.accunt.Saving;
+import com.cydeo.config.Config;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
@@ -11,7 +12,7 @@ public class Main {
 
     public static void main(String[] args) {
 
-        ApplicationContext container = new AnnotationConfigApplicationContext(ObjectInputFilter.Config.class);
+        ApplicationContext container = new AnnotationConfigApplicationContext(Config.class);
 
         Saving saving = container.getBean(Saving.class);
         Current current = container.getBean(Current.class);
