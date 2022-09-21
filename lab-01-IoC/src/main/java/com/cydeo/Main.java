2ff79b3a03174e9ca55b2384e6c55b23f206193b
lgaid <104643045+lgaid@users.noreply.github.com>
2022-09-21 18:57:21 -0400

    IoC

--- lab-01-IoC/src/main/java/com/cydeo/Main.java
+++ lab-01-IoC/src/main/java/com/cydeo/Main.java
@@ -2,11 +2,9 @@ package com.cydeo;
 
 import com.cydeo.accunt.Current;
 import com.cydeo.accunt.Saving;
-import com.cydeo.config.Config;
+import com.cydeo.lab03springboot.config.Config;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
-
-import java.io.ObjectInputFilter;
 
 public class Main {
 
