74e24ed72eb1ffe6e20235b922f18a8f6b430e59
lgaid <104643045+lgaid@users.noreply.github.com>
2022-09-27 19:50:31 -0400

    Thymeleaf

---
+++ spring-05-thymeleaf/src/test/java/com/cydeo/spring05thymeleaf/Spring05ThymeleafApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cydeo.spring05thymeleaf;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Spring05ThymeleafApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
