7ef5f9779b7c26a5a646c6b26b491ac989e5faf7
lgaid <104643045+lgaid@users.noreply.github.com>
2022-12-08 13:26:37 -0500

    ORM and REST

---
+++ lab-00-coupling/src/main/java/com/cydeo/Main.java
@@ -0,0 +1,29 @@
+package com.cydeo;
+
+import com.cydeo.tightly.BalanceService;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import java.math.BigDecimal;
+import java.util.UUID;
+
+@SpringBootApplication
+public class Main {
+
+    public static void main(String[] args) {
+        UUID user = UUID.randomUUID();
+
+        CustomerBalance customerBalance = new CustomerBalance(user, BigDecimal.ZERO);
+        GiftCardBalance giftCardBalance = new GiftCardBalance(user, BigDecimal.ZERO);
+
+        customerBalance.addBalance(new BigDecimal(150));
+        giftCardBalance.addBalance(new BigDecimal(120));
+
+        BalanceService balanceService =
+                new BalanceService(customerBalance, giftCardBalance);
+
+        System.out.println(balanceService.checkoutFromCustomerBalance(new BigDecimal(80)));
+        System.out.println(balanceService.checkoutFromGiftBalance(new BigDecimal(80)));
+    }
+
+
+}
--- lab-02-DI/src/main/java/com/cydeo/lab03springboot/service/StockService.java
+++
@@ -1,9 +0,0 @@
-package com.cydeo.lab03springboot.service;
-
-import com.cydeo.lab03springboot.model.Product;
-
-public interface StockService {
-
-    boolean checkStockIsAvailable(Product product, int quantity);
-
-}
