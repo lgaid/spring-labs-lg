287ff38b32692dbe6829b9a039c1e1ac6cdd18af
lgaid <104643045+lgaid@users.noreply.github.com>
2022-12-08 13:32:16 -0500

    IoC

---
+++ lab-01-ioc/src/test/java/com/cydeo/lab01ioc/Lab01IocApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cydeo.lab01ioc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Lab01IocApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
