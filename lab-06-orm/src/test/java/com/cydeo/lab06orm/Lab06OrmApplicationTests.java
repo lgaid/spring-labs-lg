e2f3ad7a984d288fe34a3c6d85309c7a048cd765
lgaid <104643045+lgaid@users.noreply.github.com>
2022-12-08 12:52:37 -0500

    ORM and REST

---
+++ lab-06-orm/src/test/java/com/cydeo/lab06orm/Lab06OrmApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cydeo.lab06orm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Lab06OrmApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
